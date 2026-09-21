351bec73a3c8460ef1ccb4eb685c6a390b9a1691
Adolphe <naphe12@yahoo.fr>
2026-09-21 20:22:07 +0200

    offer accepted

--- frontend/src/components/messages/MessageBubble.tsx
+++ frontend/src/components/messages/MessageBubble.tsx
@@ -2,29 +2,47 @@ import type {
   Message,
 } from "./types";
 
-
 type Props = {
   message: Message;
   mine: boolean;
 };
 
-
 export default function MessageBubble({
   message,
   mine,
 }: Props) {
+  const createdAt =
+    new Date(message.created_at);
+
   return (
     <div
       className={
         mine
-          ? "message-bubble message-bubble--mine"
-          : "message-bubble"
+          ? "message-row message-row--mine"
+          : "message-row message-row--other"
       }
     >
-      <p>{message.content}</p>
-      <span>
-        {new Date(message.created_at).toLocaleString("fr-FR")}
-      </span>
+      <div
+        className={
+          mine
+            ? "message-bubble message-bubble--mine"
+            : "message-bubble message-bubble--other"
+        }
+      >
+        <div className="message-bubble__body">
+          {message.content}
+        </div>
+
+        <div className="message-bubble__meta">
+          {createdAt.toLocaleTimeString(
+            "fr-FR",
+            {
+              hour: "2-digit",
+              minute: "2-digit",
+            },
+          )}
+        </div>
+      </div>
     </div>
   );
 }
